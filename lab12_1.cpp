0a795b9d91b48d63ba81792385d11d0827fbc062
Surabordin <bluelimec@gmail.com>
2021-12-21 16:25:45 +0700

    lab12-1 complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,20 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+
+#include<cstdlib>
+
+#include<ctime>
+
+using namespace std;
+
+int main(){
+    srand(time(0));
+    string Grade[]={"A","B+","B","C+","C","D+","D","F","W"};
+    string x = Grade[rand()%9];
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get "<< x <<" in this 261102.";
+
+}
+
